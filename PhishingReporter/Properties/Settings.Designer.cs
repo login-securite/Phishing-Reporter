b8c24f7a18c930b43b87666a7cfac989ad8d4e7a
Paul Mabileau <paul.mabileau@login-securite.com>
2023-06-06 12:23:41 +0200

    Update properties
    Signed-off-by: Paul Mabileau <paul.mabileau@login-securite.com>

--- PhishingReporter/Properties/Settings.Designer.cs
+++ PhishingReporter/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PhishingReporter.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -23,75 +23,57 @@ namespace PhishingReporter.Properties {
             }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Information Security Team <infosec@example.com>")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Login Sécurité - Support <support@login-securite.com>")]
         public string infosec_email {
             get {
                 return ((string)(this["infosec_email"]));
             }
-            set {
-                this["infosec_email"] = value;
-            }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://gophish.example.com")]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string gophish_url {
             get {
                 return ((string)(this["gophish_url"]));
             }
-            set {
-                this["gophish_url"] = value;
-            }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("3338")]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string gophish_listener_port {
             get {
                 return ((string)(this["gophish_listener_port"]));
             }
-            set {
-                this["gophish_listener_port"] = value;
-            }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("X-GOPHISH-ASMN")]
+        [global::System.Configuration.DefaultSettingValueAttribute("^")]
         public string gophish_custom_header {
             get {
                 return ((string)(this["gophish_custom_header"]));
             }
-            set {
-                this["gophish_custom_header"] = value;
-            }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Plugin Support <support@example.com>")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Login Sécurité - Support <support@login-securite.com>")]
         public string support_email {
             get {
                 return ((string)(this["support_email"]));
             }
-            set {
-                this["support_email"] = value;
-            }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("V1.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v1.0")]
         public string plugin_version {
             get {
                 return ((string)(this["plugin_version"]));
-            }
-            set {
-                this["plugin_version"] = value;
             }
         }
         
